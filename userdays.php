1f360ddecd48fef5313019b7fcbd3a19161f2001
septor <patrickweaver@gmail.com>
2010-11-14 16:35:35 -0500

    userdays support added, various tweaks

---
+++ userdays.php
@@ -0,0 +1,23 @@
+<?php
+
+$userdays = array(
+
+	array(
+		'name' => "septor's birthday",
+		'month' => 2,
+		'day' => 5,
+		'dayspan' => '',
+		'weekday' => ''
+	),
+
+	array(
+		'name' => "Date+ born (2010)",
+		'month' => 11,
+		'day' => 13,
+		'dayspan' => '',
+		'weekday' => ''
+	),
+
+);
+
+?>
